907519118551dc1ca63b5317a87df29883827382
kowany <hvilla65@gmail.com>
2022-03-23 15:40:02 -0600

    Validamos las variables de ambientes en el ConfigModule de AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
-import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+
+import * as Joi from 'joi';
+
+import { AppController } from './app.controller';
 
 import { AppService } from './app.service';
 
@@ -14,12 +17,18 @@ import { lastValueFrom } from 'rxjs';
 import { environments } from './environments';
 import config from './config';
 
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: environments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     ProductsModule,
     UsersModule,
