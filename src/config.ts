047c9880d7ff9c8eb7bb48d089035f2c7e305ecc
kowany <hvilla65@gmail.com>
2022-03-23 11:45:12 -0600

    Creado archivo config.ts

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('config', () => {
+  return {
+    database: {
+      database: {
+        name: process.env.DATABASE_NAME,
+        port: process.env.DATABASE_PORT,
+      },
+      apiKey: process.env.API_KEY,
+    },
+  };
+});
