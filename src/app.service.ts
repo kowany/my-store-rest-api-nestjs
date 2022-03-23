e2ad9eb594a81c34af89b7de25df0680e793a5e5
kowany <hvilla65@gmail.com>
2022-03-23 12:03:31 -0600

    En lugar del ConfigService utilizamos el ConfigType e inyectamos el config.KEY

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,17 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
-    // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
-    private configService: ConfigService,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
   getHello(): string {
-    const apiKey = this.configService.get<string>('API_KEY');
-    const dbName = this.configService.get<string>('DATABASE_NAME');
+    const apiKey = this.configService.apiKey;
+    const dbName = this.configService.database.name;
     console.log(this.tasks);
     return `Hello World! ${apiKey} ${dbName}`;
   }
